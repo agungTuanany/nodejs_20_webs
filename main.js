9595f00b52544182a32bac33d714c0c8ec29afac
agungTuanany <agung.tuanany@gmail.com>
2020-07-27 13:14:51 +0700

    Parsing request headers

--- main.js
+++ main.js
@@ -18,13 +18,22 @@ const server = http.createServer((req, res) => {
     // Get the query string as an object
     const queryStringObject = JSON.parse(JSON.stringify(parsedUrl.query)) // parsedUrl.query
 
+    // Get the headers as an object
+    const header            = req.headers
+
     res.end ("Hello world\n")
 
     // log the request path
     console.log(`Request recieved on path: "${trimmedPath}"`)
     console.log(`Request recieved method: "${method}"`)
     console.log("Query string parameters:", queryStringObject)
+    console.log("Request recieved headers:", header)
+
+    // The request on browser
+    // http://localhost:8082/app.js?foo=bad&baz=foo
 
 })
 
 server.listen (port, () => console.log(`The server is listening on port ${port}`))
+
+
