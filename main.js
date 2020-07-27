1d8b4366465ec1984800c272e5018f5fdd35bb01
agungTuanany <agung.tuanany@gmail.com>
2020-07-27 14:49:15 +0700

    Routing request | create handlers

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
     const method            = req.method.toLowerCase()
 
     // Get the query string as an object
-    const queryStringObject = JSON.parse(JSON.stringify(parsedUrl.query)) // parsedUrl.query
+    const queryStringObject =  parsedUrl.query //JSON.parse(JSON.stringify(parsedUrl.query))
 
     // Get the headers as an object
     const header            = req.headers
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
     const decoder           = new StringDecoder("utf-8")
     let buffer              = "" // Just placeholder for string
 
-    // Using stream
+    // Using stream | emit stream
     req.on("data", data => {
         buffer += decoder.write(data)
     })
@@ -34,22 +34,82 @@ const server = http.createServer((req, res) => {
     req.on("end", () => {
         buffer += decoder.end()
 
-        res.end ("Hello world\n")
+        // Choose the handlers this request should go to if request is not
+        // found, use notFound handlers
+        const choosenHandler = typeof(router[trimmedPath]) !== "undefined" ? router[trimmedPath] : handlers.notFound
 
-        // log the request path
-        console.log(`Request recieved on path: "${trimmedPath}"`)
-        console.log(`Request recieved method: "${method}"`)
-        console.log("Query string parameters:", queryStringObject)
-        console.log("Request recieved headers:", header)
-        console.log("Request recieved payload", buffer)
+
+        // Construct the data object to send to the handler
+        let data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            header,
+            "payload": buffer
+        }
+
+        // Route the request to the handler specified in the router
+        choosenHandler(data, (statusCode, payload) => {
+            statusCode = typeof(statusCode) === "number" ? statusCode : 200
+
+            payload    = typeof(payload) === "object" ? payload : {}
+
+            // Convert the payload into a string
+            const payloadString = JSON.stringify(payload)
+
+            // Return the response
+            res.writeHead(statusCode)
+            res.end(payloadString)
+
+            // log the request path
+            console.log("Returning response: ", statusCode, payloadString)
+
+        })
+        // console.log(`Request recieved on path: "${trimmedPath}"`)
+        // console.log(`Request recieved method: "${method}"`)
+        // console.log("Query string parameters:", queryStringObject)
+        // console.log("Request recieved headers:", header)
+        // console.log("Request recieved payload", buffer)
+
+        // The request on browser
+        // http://localhost:8082/app.js?foo=bad&baz=foo
     })
-
-
-    // The request on browser
-    // http://localhost:8082/app.js?foo=bad&baz=foo
-
 })
 
 server.listen (port, () => console.log(`The server is listening on port ${port}`))
 
+// Define the handlers
+let handlers = {}
 
+// Sample handlers
+handlers.about = (data, callback) => {
+    // Callback an HTTP status code, and payload object
+    callback(200, {
+        "name"  : "About handlers",
+        "Group" : "Home"
+    })
+}
+
+handlers.projects = (data, callback) => {
+    callback(200, {
+        "name"  : "Project handlers",
+        "Group" : "Project"
+    })
+}
+
+handlers.blog = (data, callback) => {
+    callback(200, {
+        "name"  : "Blog handlers",
+        "Group" : "Blog"
+    })
+}
+
+// Not found handlers
+handlers.notFound = (data, callback) => callback(404)
+
+// Define a reqest router
+const router = {
+    "about"    : handlers.about,
+    "projects" : handlers.projects,
+    "blog"     : handlers.blog
+}
